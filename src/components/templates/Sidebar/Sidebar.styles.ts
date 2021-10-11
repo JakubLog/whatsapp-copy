8c5c804a2aa922b24c78f446337625671250630c
JaFe <kontakt.jakubfedoszczak@gmail.com>
2021-10-11 06:12:44 +0200

    Add searchbar organism

--- src/components/templates/Sidebar/Sidebar.styles.ts
+++ src/components/templates/Sidebar/Sidebar.styles.ts
@@ -3,6 +3,6 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   display: grid;
   grid-template-columns: 100%;
-  grid-template-rows: 55px 45px auto;
+  grid-template-rows: 55px 50px auto;
   border-right: 1px solid #ccc;
 `;
