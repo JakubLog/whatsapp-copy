91135d2573f89a7785171c47a3117a124db932c5
JaFe <kontakt.jakubfedoszczak@gmail.com>
2021-10-22 05:19:17 +0200

    Add input handling by React-hook-form

--- src/components/organisms/SendPlace/SendPlace.tsx
+++ src/components/organisms/SendPlace/SendPlace.tsx
@@ -1,14 +1,21 @@
 import Icon from 'components/atoms/Icon/Icon';
 import React from 'react';
 import { Wrapper, StyledInput } from './SendPlace.styles';
+import { useForm } from 'react-hook-form';
 
 const SendPlace: React.FC = () => {
+  const { handleSubmit, register } = useForm();
+
+  const process = ({ value }: { value: string }) => {
+    console.log(value);
+  };
+
   return (
     <Wrapper>
       <Icon src="https://www.pngfind.com/pngs/m/220-2207946_png-file-svg-white-emoji-icon-png-transparent.png" />
       <Icon src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Ic_attach_file_48px.svg/1200px-Ic_attach_file_48px.svg.png" />
-      <form>
-        <StyledInput placeholder="Wpisz tutaj wiadomość..." />
+      <form onSubmit={handleSubmit(process)}>
+        <StyledInput placeholder="Wpisz tutaj wiadomość..." {...register('value', { required: true })} />
         <Icon src="https://static.thenounproject.com/png/488906-200.png" />
       </form>
     </Wrapper>
