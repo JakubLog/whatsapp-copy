916a808584843a76f7ede6ab3b3fde488734e94f
JaFe <kontakt.jakubfedoszczak@gmail.com>
2021-11-12 19:01:26 +0100

    Add another test to contacts integration test

--- src/e2e/cypress/integration/Contacts.test.ts
+++ src/e2e/cypress/integration/Contacts.test.ts
@@ -9,7 +9,7 @@ describe('Whatsapp copy | Application', () => {
     cy.wait(2000);
     signInWithEmailAndPassword(auth, 'test123@test.com', 'test123456');
   });
-  it('Checks is contacts mechanisms working', () => {
+  it('Checks is contacts add/remove mechanisms works', () => {
     cy.findByTestId('optionsForProfile').click().findByTestId('createMessage').click();
     cy.findAllByPlaceholderText('Fullname').last().type('Jakub Michał Fedoszczak', { force: true });
     cy.findAllByText("Let's talk!").last().click({ force: true });
@@ -18,5 +18,28 @@ describe('Whatsapp copy | Application', () => {
     cy.findByText('Jakub Michał Fedoszczak').parent().click();
     cy.findByTestId('optionsForContact').click().findByTestId('removeContact').should('exist').click();
     cy.findByText('Jakub Michał Fedoszczak').should('not.exist');
+    cy.wait(2000);
+  });
+  it('Checks is contacts toggling mechanisms works', () => {
+    cy.reload();
+    cy.findByTestId('optionsForProfile').click().findByTestId('createMessage').click();
+    cy.findAllByPlaceholderText('Fullname').last().type('Jakub Michał Fedoszczak', { force: true });
+    cy.findAllByText("Let's talk!").last().click({ force: true });
+    cy.wait(2000);
+    cy.reload();
+    cy.wait(2000);
+    cy.findByTestId('optionsForProfile').click().findByTestId('createMessage').click();
+    cy.findAllByPlaceholderText('Fullname').last().type('Kuba Menel', { force: true });
+    cy.findAllByText("Let's talk!").last().click({ force: true });
+    cy.wait(2000);
+    cy.findByTestId('closeSlider').click();
+    cy.findByText('Jakub Michał Fedoszczak').parent().click();
+    cy.findByText('Kuba Menel').parent().click();
+    cy.findByText('Jakub Michał Fedoszczak').parent().click();
+    cy.findByTestId('optionsForContact').click().findByTestId('removeContact').should('exist').click();
+    cy.findByText('Kuba Menel').parent().click();
+    cy.findByTestId('optionsForContact').click().findByTestId('removeContact').should('exist').click();
+    cy.findByText('Jakub Michał Fedoszczak').should('not.exist');
+    cy.findByText('Kuba Menel').should('not.exist');
   });
 });
